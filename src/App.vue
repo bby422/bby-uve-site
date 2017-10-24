c026b7759cbe41d0eb21cdeef52dd94fff11296e
guo.r <guo.r@yougou.com>
2017-10-24 19:26:08 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    {{msg}}
+    <cbutton href="http://www.baidu.com" title="哈哈" text="点呀" />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import cbutton from './components/CButton'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: '你个傻逼孩子'
+    }
+  },
+  components: {
+    cbutton
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CButton.vue
@@ -0,0 +1,21 @@
+<template>
+	<a class="btn-default" v-bind:href="href" v-bind:title="title" @click="taClick">{{text}}</a>
+</template>
+<script>
+export default {
+  name: 'cbutton',
+  data () {
+    return {
+      href: '#',
+      title: '',
+      text: '确定'
+    }
+  },
+  methods: {
+    taClick: function (e) {
+      e.preventDefault()
+       this.text = 'OK'
+    }
+  }
+}
+</script>
